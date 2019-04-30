f6b829037a5c11d552e75d32e512808aed46036e
Jonathan Scholz <contact@jonathan-scholz.de>
2019-04-30 23:15:30 +0200

    refactor(tester/expect): universal expectation creation method

--- tester/expectation/expectation.go
+++ tester/expectation/expectation.go
@@ -1,9 +1,44 @@
 package expectation
 
 import (
+	"encoding/json"
+	"github.com/pkg/errors"
 	"github.com/robojones/gqltest/tester/request"
+	"github.com/vektah/gqlparser/ast"
 )
 
 type Expectation interface {
 	Check(result *request.Result) error
+}
+
+func FromDirective(path []string, directive *ast.Directive) (Expectation, error) {
+	switch directive.Name {
+	case "expect":
+		if len(directive.Arguments) < 1 {
+			return nil, errors.New("missing argument v")
+		}
+		value := directive.Arguments[0].Value
+
+		raw := value.Raw
+		v := new(interface{})
+
+		switch value.Kind {
+		case ast.Variable:
+			return nil, errors.New("variables not supported")
+		case ast.FloatValue, ast.IntValue, ast.BooleanValue, ast.ListValue, ast.ObjectValue, ast.NullValue:
+			err := json.Unmarshal([]byte(raw), v)
+			if err != nil {
+				panic(errors.Wrap(err, "parsing argument value"))
+			}
+			break
+		case ast.StringValue, ast.EnumValue:
+			var inter interface{} = raw
+			v = &inter
+			break
+		}
+
+		return NewValueExpectation(path, v), nil
+	}
+
+	return nil, nil
 }
